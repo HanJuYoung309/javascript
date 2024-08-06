639ad1735ef29fc6641ed9c0f19617ed3b797e18
HanJuYoung309 <juyg407@naver.com>
2024-08-06 21:16:40 +0900

    init

---
+++ test.js
@@ -0,0 +1,17 @@
+//함수
+
+let num1=10;
+let num2=15;
+
+let sum= num1+num2;
+
+console.log(sum);
+
+function add(num1,num2){
+
+   return num1+num2;
+   console.log("함수 호출");
+}
+add(10,15);
+
+//console.log('두 숫자를 더한 결과는 ')
